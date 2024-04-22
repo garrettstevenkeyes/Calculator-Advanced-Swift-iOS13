45a4c5e2e9373fd96cc4aa9aafc3958ede671482
Garrett Keyes <garrett.keyes@gmail.com>
2024-04-21 21:17:30 -0400

    finished calculator

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,20 +10,50 @@ import Foundation
 
 struct CalculatorLogic {
     
-    var number: Double
+    private var number: Double?
     
-    init(number: Double) {
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+
+    mutating func setNumber(_ number: Double){
         self.number = number
     }
-
-    func calculate(symbol: String)-> Double? {
-        if symbol == "+/-" {
-            return number * -1
-        } else if symbol == "AC" {
-            return 0
-        } else if symbol == "%" {
-            return number * 0.01
+    
+    mutating func calculate(symbol: String)-> Double? {
+        
+        if let n = number {
+            if symbol == "+/-" {
+                return n * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return n * 0.01
+            }  else if symbol == "=" {
+                return performTwoNumberCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
         }
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1,
+            let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+                case "+":
+                    return n1 + n2
+                case "-":
+                    return n1 - n2
+                case "×":
+                    return n1 * n2
+                case "÷":
+                    return n1 / n2
+                default:
+                    fatalError("The operation passed in does not match any of the cases")
+            }
+        }
+        
         return nil
     }
 }
